b8f2cecedffcde68fc7e411f94a3dbf407e19288
Nirmesh Rastogi <nirmesh.rastogi@dish.com>
2023-01-21 20:56:19 +0530

    made changes

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'postgres',
-  host: 'localhost',
+  host: '172.30.104.79',
   database: 'postgres',
   password: 'root',
   port: 5432,
