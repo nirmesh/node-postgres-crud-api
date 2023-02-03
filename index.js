33e86fabb4300c26a7a28763c92fb735c668129e
Rastogi <Nirmesh.Rastogi@dell.com>
2023-02-03 18:52:52 +0530

    added changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
 );
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API developed by Dell' });
+  response.json({ info: `Node.js, Express, and Postgres API developed by Nirmesh. Postgres host address::${process.env.DB_HOST}` });
 });
 
 app.get('/users', db.getUsers);
